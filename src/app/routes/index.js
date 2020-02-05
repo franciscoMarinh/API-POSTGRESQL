32397fda6c2ac93b6df152284b05d2d8e9f1f360
francisco-marinho <franc1sc1566@gmail.com>
2020-02-05 14:31:41 -0300

    Page notFound implemented

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -12,6 +12,20 @@ fs.readdirSync(__dirname)
         routes.use(require(`${__dirname}/${file}`))
     });
 
+routes.use((req, res, next) => {
+    res.status(404).json({
+        error: "Page notFound",
+        avaliableRoutes: {
+            Methods: {
+                GET: ["/user"],
+                POST: ['/register', '/auth'],
+                PUT: ['/user'],
+                DELETE: ['/user']
+            }
+        }
+    })
+})
+
 routes.use(ErrorMiddleware)
 
 module.exports = routes

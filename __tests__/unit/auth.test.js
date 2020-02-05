24d680a7592eb2dea79012e455eb3b13cad24482
francisco-marinho <franc1sc1566@gmail.com>
2020-02-05 13:05:03 -0300

    First deployment

--- __tests__/unit/auth.test.js
+++ __tests__/unit/auth.test.js
@@ -15,15 +15,28 @@ describe('User', () => {
             password: "123456789"
         })
         const token = await user.genToken()
+<<<<<<< HEAD
         const response = await request(app).get('/user')
             .set('authorization', `Bearer ${token}`)
+=======
+        const response = await request.get('/user')
+            .set('Authorization', `Bearer ${token}`)
+            
+>>>>>>> e06839acf41cbb7709811658762e97116c5d266d
         expect(response.statusCode).toBe(200)
+        expect(response.body.email).toEqual(user.dataValues.email)
     })
+
     test('Verificando rejeição de token incorreto', async () => {
         const wrongToken = jwt.sign({ id: 20 }, 'wrongToken')
+<<<<<<< HEAD
         const response = await request(app).get('/user')
             .set('authorization', `Bearer ${wrongToken}`)
 
+=======
+        const response = await request.get('/user')
+            .set('Authorization', `Bearer ${wrongToken}`)
+>>>>>>> e06839acf41cbb7709811658762e97116c5d266d
         expect(response.status).toBe(401)
     })
 
